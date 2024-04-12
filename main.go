43d4c0186d1a6dd68d95249bc857fc3657b09d4a
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-04-12 20:16:03 +0600

    1

--- main.go
+++ main.go
@@ -20,9 +20,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+func youtrack(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "GET" {
+		http.Error(w, "Only GET requests are supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Println("ok")
+	res, _ := json.Marshal(Response{Message: "ok"})
+	w.Write(res)
+}
+
 func main() {
 	port := "80"
 	http.HandleFunc("/", index)
+	http.HandleFunc("/youtrack", youtrack)
 	fmt.Println("Server running on http://localhost:" + port)
 	http.ListenAndServe(":"+port, nil)
 }
